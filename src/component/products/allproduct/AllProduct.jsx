3d548019009380c4ed19f4fbe31f558e49025666
Ata Ahmad <ataahmad72600@gmail.com>
2024-03-26 23:11:00 +0530

    update

--- src/component/products/allproduct/AllProduct.jsx
+++ src/component/products/allproduct/AllProduct.jsx
@@ -40,25 +40,30 @@ const AllProduct = () => {
   };
 
   return (
-    <div className="flex flex-row">
+    <div className="flex flex-row bg-gray-100">
       {/* Sidebar */}
-      <div className="bg-gray-800 text-white w-1/3 md:w-1/5 flex-none">
-        <div className="p-4">
-          <h1 className="md:text-3xl font-mono">Our Product</h1>
+      <div data-aos="fade-up" className=" text-black w-1/3 md:w-1/5 flex-none" >
+        <div className='rounded-lg border p-2 m-2 bg-white' style={{
+        boxShadow: '8px 8px lightblue'
+      }}>
+        <div className=" text-center">
+          <h1 className="md:text-4xl text-black font-fira-neue font-semibold">Our Product</h1>
         </div>
-        <ul className="md:py-4">
+        <ul className="md:py-4  ">
           {productComponents.map((product) => (
             <li key={product.name}>
               <NavLink
                 to={`/cosmeceuticals/#${product.name}`}
                 onClick={() => handleProductClick(product.name)}
-                className="block px-4 py-2 text-xs md:text-base hover:bg-gray-700"
+                className="block px-4 py-2 text-xs md:text-sm m-4 hover:text-white border rounded-sm border-black hover:bg-black "
+                target=''
               >
                 {product.name}
               </NavLink>
             </li>
           ))}
         </ul>
+      </div>
       </div>
       {/* Main Content */}
       <div className="flex-1">

cbeefc56f1b17cb12e5ad23b263e0974a39740ee
Ata Ahmad <ataahmad72600@gmail.com>
2024-03-26 11:59:49 +0530

    update

--- src/component/products/Extract.jsx
+++ src/component/products/Extract.jsx
@@ -97,10 +97,10 @@ const Extract = () => {
           {b.map((category, index) => {
             return (
               <div key={index} className="pl-[25px] pr-[25px] mt-4">
-                <div className="text-center pb-4">
+                <div className="text-center md:pb-4 pb-2">
                   <span
                     id={Object.keys(category)[0]}
-                    className="pl-1 border-b-2 font-semibold border-black text-4xl"
+                    className="pl-1 border-b-2 font-semibold border-black md:text-4xl"
                   >
                     {Object.keys(category)[0]}
                   </span>
@@ -108,8 +108,8 @@ const Extract = () => {
                 {category[Object.keys(category)[0]].map((ele, index) => {
                   return (
                     <div key={index} className="bg-green-400 hover:text-black hover:bg-green-200 mt-2">
-                      <li className="flex justify-between products-center pl-1 py-2">
-                        <span className="text-lg font-semibold hover:font-bold">{ele.product}</span>
+                      <li className="flex justify-between products-center pl-1 md:py-2 py-1">
+                        <span className="md:text-lg text-sm md:font-semibold hover:font-bold">{ele.product}</span>
                       </li>
                     </div>
                   );
--- src/component/products/Fixatives.jsx
+++ src/component/products/Fixatives.jsx
@@ -23,28 +23,33 @@ const Fixatives = () => {
         <div className="md:w-full md:flex  ">
           <div className=" md:w-full ">
           {b.map((category, index) => {
-            return (
-              <div key={index} className="pl-[25px] pr-[25px] mt-4">
-                <div className="text-center pb-4">
-                  <span
-                    id={Object.keys(category)[0]}
-                    className="pl-1 border-b-2 font-semibold border-black text-4xl"
-                  >
-                    {Object.keys(category)[0]}
-                  </span>
+              return (
+                <div key={index} className="pl-[25px] pr-[25px] mt-4">
+                  <div className="text-center md:pb-4 pb-2">
+                    <span
+                      id={Object.keys(category)[0]}
+                      className="pl-1 border-b-2 font-semibold border-black md:text-4xl"
+                    >
+                      {Object.keys(category)[0]}
+                    </span>
+                  </div>
+                  {category[Object.keys(category)[0]].map((ele, index) => {
+                    return (
+                      <div
+                        key={index}
+                        className="bg-green-400 hover:text-black hover:bg-green-200 mt-2"
+                      >
+                        <li className="flex justify-between products-center pl-1 md:py-2 py-1">
+                          <span className="md:text-lg text-sm md:font-semibold hover:font-bold">
+                            {ele.product}
+                          </span>
+                        </li>
+                      </div>
+                    );
+                  })}
                 </div>
-                {category[Object.keys(category)[0]].map((ele, index) => {
-                  return (
-                    <div key={index} className="bg-green-400 hover:text-black hover:bg-green-200 mt-2">
-                      <li className="flex justify-between products-center pl-1 py-2">
-                        <span className="text-lg font-semibold hover:font-bold">{ele.product}</span>
-                      </li>
-                    </div>
-                  );
-                })}
-              </div>
-            );
-          })}
+              );
+            })}
           </div>
         </div>
       </div>

d08acd519a3e2ff33eaf95c11e994338e0f89442
Ata Ahmad <ataahmad72600@gmail.com>
2024-03-28 13:58:25 +0530

    update

--- src/component/products/ConditioningAgent.jsx
+++ src/component/products/ConditioningAgent.jsx
@@ -42,14 +42,16 @@ const ConditioningAgent = () => {
           <div className=" md:w-full ">
           {b.map((category, index) => {
               return (
-                <div key={index} className="pl-[25px] pr-[25px] md:mt-[24px] mt-10 m-5 pb-5 pt-2 border rounded-md  bg-gray-50" style={{
-                  boxShadow: "0 4px 6px rgba(0, 0, 0, 0.2)",
+                <div key={index} className="pl-[25px] pr-[25px] md:mt-[24px] mt-10 m-5 pb-5 pt-2 border rounded-md border-black   bg-gray-50 " data-aos="fade-up-left" style={{
+                  // boxShadow:"10px 10px lightblue"
+                  boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+
 
                 }} >
                   <div className="text-center md:pb-4 pb-2 ">
                     <span
                       id={Object.keys(category)[0]}
-                      className="pl-1 border-b-2 font-semibold font-fira-neue border-black md:text-4xl"
+                      className="pl-1  font-semibold font-fira-neue  md:text-4xl"
                     >
                       {Object.keys(category)[0]}
                     </span>
@@ -58,9 +60,9 @@ const ConditioningAgent = () => {
                     return (
                       <div
                         key={index}
-                        className="bg-cyan-100 hover:text-black border rounded-sm border-black hover:bg-green-200 mt-2"
+                        className="bg-white hover:text-white border rounded-sm border-black hover:bg-black mt-2"
                       >
-                        <li className="flex justify-between products-center pl-1 md:py-2 py-1">
+                        <li className="flex justify-between products-center pl-3 md:py-2 py-1">
                           <span className="md:text-lg text-sm md:font-semibold hover:font-bold">
                             {ele.product}
                           </span>
--- src/component/products/Milliglobules.jsx
+++ src/component/products/Milliglobules.jsx
@@ -171,14 +171,16 @@ const Milliglobules = () => {
           <div className=" md:w-full ">
           {b.map((category, index) => {
               return (
-                <div key={index} className="pl-[25px] pr-[25px] md:mt-[24px] mt-10 m-5 pb-5 pt-2 border rounded-md  bg-gray-50" style={{
-                  boxShadow: "0 4px 6px rgba(0, 0, 0, 0.2)",
+                <div key={index} className="pl-[25px] pr-[25px] md:mt-[24px] mt-10 m-5 pb-5 pt-2 border rounded-md border-black   bg-gray-50 " data-aos="fade-up-left" style={{
+                  // boxShadow:"10px 10px lightblue"
+                  boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+
 
                 }} >
                   <div className="text-center md:pb-4 pb-2 ">
                     <span
                       id={Object.keys(category)[0]}
-                      className="pl-1 border-b-2 font-semibold font-fira-neue border-black md:text-4xl"
+                      className="pl-1  font-semibold font-fira-neue  md:text-4xl"
                     >
                       {Object.keys(category)[0]}
                     </span>
@@ -187,9 +189,9 @@ const Milliglobules = () => {
                     return (
                       <div
                         key={index}
-                        className="bg-cyan-100 hover:text-black border rounded-sm border-black hover:bg-green-200 mt-2"
+                        className="bg-white hover:text-white border rounded-sm border-black hover:bg-black mt-2"
                       >
-                        <li className="flex justify-between products-center pl-1 md:py-2 py-1">
+                        <li className="flex justify-between products-center pl-3 md:py-2 py-1">
                           <span className="md:text-lg text-sm md:font-semibold hover:font-bold">
                             {ele.product}
                           </span>

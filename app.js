77c73280142d32fec1bb6177df271e1718af5e86
Ata Ahmad <ataahmad72600@gmail.com>
2024-04-30 19:59:37 +0530

    update port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express')
 const path = require("path")
 const app = express();
-const port = 8006;
+const port = process.env.PORT;
 const router = require('./routes/router')
 const cors = require('cors')
 app.use(express.json());
